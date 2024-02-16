13e955ad6b55921b1583e1f18c4c476b15bb0b14
demand <demand@yandex.ru>
2024-02-16 16:31:46 +0200

    Optimize imports

--- kfreq-api/src/main/java/epassi/kfreq/app/KFreqApp.java
+++ kfreq-api/src/main/java/epassi/kfreq/app/KFreqApp.java
@@ -1,11 +1,8 @@
 package epassi.kfreq.app;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 
--- kfreq-api/src/main/java/epassi/kfreq/service/KFreqCache.java
+++ kfreq-api/src/main/java/epassi/kfreq/service/KFreqCache.java
@@ -3,7 +3,6 @@ package epassi.kfreq.service;
 import epassi.kfreq.model.FrequencyRecord;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.WeakHashMap;
