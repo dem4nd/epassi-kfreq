9752aa12d8240a6b63761366ee4f4dc91236a4b2
demand <demand@yandex.ru>
2024-02-16 14:36:31 +0200

    Fix example in API doc

--- kfreq-api/src/main/java/epassi/kfreq/model/FrequencyRecord.java
+++ kfreq-api/src/main/java/epassi/kfreq/model/FrequencyRecord.java
@@ -5,9 +5,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Word occurance count")
 public record FrequencyRecord(
-    @Schema(description = "Counted word")
+    @Schema(description = "Counted word", example = "example")
     String word,
-    @Schema(description = "The number of these words in the text")
+    @Schema(description = "The number of these words in the text", example = "3")
     int count) implements Comparable<FrequencyRecord> {
 
   @Override
